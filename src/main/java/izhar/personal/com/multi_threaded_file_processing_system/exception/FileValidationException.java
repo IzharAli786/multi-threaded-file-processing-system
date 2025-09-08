bed92f4a61bbb555e8042e7bb8e9b79036f4b609
IzharAli786 <faradayeinstien@gmail.com>
2025-09-08 20:27:11 +0500

    feat: implement resilient file processing with validation and conversion timeout handling

---
+++ src/main/java/izhar/personal/com/multi_threaded_file_processing_system/exception/FileValidationException.java
@@ -0,0 +1,15 @@
+package izhar.personal.com.multi_threaded_file_processing_system.exception;
+
+public class FileValidationException extends FileProcessingException{
+    private String validationType;
+
+    public FileValidationException(String fileName, String validationType,String message) {
+        super(fileName,"Validate",message);
+        this.validationType = validationType;
+    }
+
+    public String getValidationType() {
+        return validationType;
+    }
+
+}
